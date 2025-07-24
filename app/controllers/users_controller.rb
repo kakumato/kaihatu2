34ff68c5a2542abcd93a01f739195c195dd08386
ishibashi <r6_ishibashi@icc.core.ac.jp>
2025-07-24 15:24:05 +0900

    ユーザー登録成功時のメッセージ,show直し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         log_in @user
-        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.html { redirect_to @user, notice: "ユーザーを作成しました" }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
